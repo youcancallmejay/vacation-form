224d816a468be84bb8f1b321ed923f4d1b8a837c
youcancallmejay <148828634+youcancallmejay@users.noreply.github.com>
2025-01-26 23:19:11 -0600

    add form and duration selector

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import logo from './logo.svg';
+import { useState } from "react";
+
 import './App.css';
 
 function App() {
@@ -12,11 +13,22 @@ function App() {
 }
 
 function Form(){
+
+  const [destination, setDestination] = useState("");
+  const [duration, setDuration] = useState(1)
+
+  function handleSubmit(){
+
+  }
+
   return(
     <div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <h3>Complete your vacation inquiry</h3>
         <input type='text' placeholder='Destination?'></input>
+        <select value={duration}>
+         {Array.from({length: 14}, (_, i) => i+1).map(num => <option>{num }</option> )}
+        </select>
         <button>Submit</button>
       </form>
     </div>

db261aa3c73580223ea1cb4c16a5d204cd355952
youcancallmejay <148828634+youcancallmejay@users.noreply.github.com>
2025-01-28 23:29:10 -0600

    add delete function. add contact form

--- src/App.js
+++ src/App.js
@@ -10,51 +10,100 @@ function App() {
     setVacationList((vacationList) => [...vacationList, item])
   }
 
+  function handleDeleteItem(id){
+    setVacationList((vacationList) => vacationList.filter((item) => item.id != id ))
+  }
+
   return (
     <div className="App">
       <header className="App-header">
       <Form onAddItems={handleAddItems}/>
-      <VacationList list={vacationList} />
+      <DisplayList list={vacationList} onDeleteItem={handleDeleteItem}/>
+      <ContactForm />
       </header>
     </div>
   );
 }
 
-function VacationList({list}){
+
+
+function ContactForm(){
+ 
   return(
     <div>
-      <li>
-        {list.destination} {list.budget} {list.duration}
-      </li>
+      <h3>Enter your contact information</h3>
+      <form  className="form1">
+        <input type="text" placeholder="First and Last Name"></input>
+        <p>
+          <label>
+            <input type='radio' value="option1" name="myRadio"/>
+            Male
+          </label>
+
+          <label>
+            <input type='radio' value="option2" name="myRadio" />
+            Female
+          </label>
+        </p>
+
+        
+      </form>
+    </div>
+  )
+
+}
+
+
+
+function DisplayList({list, onDeleteItem}){
+
+  return(
+    <div>
+      <ul>
+        {list.map((item) => 
+          (
+          <li key={item.id}>
+            <button onClick={()=> onDeleteItem(item.id)}>Delete</button>
+            Destination: {item.destination} Duration: {item.duration} Budget: ${item.budget} Insurace: {item.insurance === true ? "Yes" : "No"}
+          </li>)
+      
+      )}
+      </ul>
     </div>
   )
 }
+
 
 function Form({onAddItems}){
 
   const [destination, setDestination] = useState("");
   const [duration, setDuration] = useState(1);
   const [budget, setBudget] = useState();
+  const [insurance, setInsurance] = useState(false) 
 
   function handleSubmit(e){
       e.preventDefault();
       if(!destination) return;
 
-      const newItem = {destination, duration, budget, insurace: false, id: Date.now()}
+      const newItem = {destination, duration, budget, insurance, id: Date.now()}
       console.log(newItem);
       onAddItems(newItem)
+      setDestination("");
+      setDuration(1);
+      setBudget('')
+      setInsurance(false)
     }
 
   return(
     <div>
       <form onSubmit={handleSubmit} className="form1">
         <h3>Complete your vacation inquiry</h3>
-        <input type='text' placeholder='Destination?'></input>
+        <input type='text' placeholder='Destination?' value={destination} onChange={(e) => setDestination(e.target.value)} ></input>
         <select value={duration} onChange={(e) => setDuration(e.target.value)}>
-         {Array.from({length: 14}, (_, i) => i+1).map(num => <option>{num }</option> )}
+         {Array.from({length: 14}, (_, i) => i+1).map(num => <option value={num} key={num}>{num}</option> )}
         </select>
-        <input type="text" placeholder="Budget?"></input>
-        <label><input type="checkbox"></input>Will you get insurance?</label>
+        <input type="text" placeholder="Budget?" value={budget} onChange={(e) => setBudget(e.target.value)}></input>
+        <label><input type="checkbox" checked={insurance} onChange={(e) => setInsurance(e.target.checked)}></input>Will you get insurance?</label>
         
         <button>Submit</button>
       </form>
